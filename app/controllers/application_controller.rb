d40892b865d8d5a450832dcc5c5acb49c96cc529
KakumaHisae <mabel8mapel@yahoo.co.jp>
2021-03-21 13:40:46 +0000

    3/21

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,5 +16,12 @@ class ApplicationController < ActionController::Base
     !current_user.nil?
   end
   
+  def authenticate_user
+    if @current_user == nil
+      flash[:notice]="ログインが必要です"
+      redirect_to login_path
+    end
+  end  
+  
   
 end
