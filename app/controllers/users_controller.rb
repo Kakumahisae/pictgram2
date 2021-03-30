5fa1f6f7c1593c87e26190949ef835cae273c3ed
KakumaHisae <mabel8mapel@yahoo.co.jp>
2021-03-30 01:17:00 +0000

    3/30

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,6 +31,11 @@ class UsersController < ApplicationController
     end  
   end  
     
+    
+  def show
+    @user = User.find_by(id: params[:id])
+    @comments = @user.comments
+  end  
   
   private
   
